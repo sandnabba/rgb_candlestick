4b8854755245b2fbf99f7fc0f59c9a05448d1dbb
Emil@DvorakMobile <Emil@DvorakMobile>
2017-12-04 08:56:28 +0100

    Adding install.sh

---
+++ controller/install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+mkdir /opt/candlestick_controller
+cp -rv http_server/ \
+       	master.py \
+       	rgb_serial.py \
+	serial_controller/ \
+       	/opt/candlestick_controller/
+
+cp -rv webapp/build/ /opt/candlestick_controller/webapp
