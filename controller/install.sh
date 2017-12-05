4b7d0f5960c849c48bf4f56eb5c7c9b244f0bef3
emil@DvorakHome <emil@DvorakHome>
2017-12-05 21:59:26 +0100

    Set permissions on webapp

--- controller/install.sh
+++ controller/install.sh
@@ -8,3 +8,5 @@ cp -rv http_server/ \
        	/opt/candlestick_controller/
 
 cp -rv webapp/build/ /opt/candlestick_controller/webapp
+chmod -R 755 /opt/candlestick_controller/webapp
+chmod -R 755 /opt/candlestick_controller
